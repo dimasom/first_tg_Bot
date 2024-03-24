dcccd644ed776788ba399aa381e87687d1dbaa59
dimasom <9117671455@mail.ru>
2024-03-24 18:51:04 +0300

    first commit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import telebot
+from telebot import types
 import os
 from dotenv import load_dotenv
 load_dotenv()
@@ -10,7 +11,21 @@ def square(num):
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, f"Hello, {message.from_user.first_name} {message.from_user.last_name}.How are you doing?")
+    markup = types.InlineKeyboardMarkup()
+    button1 = types.InlineKeyboardButton("открыть pdf", callback_data='open_pdf')
+    button2 = types.InlineKeyboardButton("открыть pdf", callback_data='open_pdf')
+    markup.row(button1,button2)
+    bot.reply_to(message, f"Привет, {message.from_user.first_name} {message.from_user.last_name}.Выбери нужное", reply_markup=markup)
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_query(call):
+    if call.data == 'open_pdf':
+        with open('call.txt', 'w')as file:
+            file.write(str(call))
+        with open('1.pdf', 'rb') as pdf_file:
+            bot.send_document(call.message.chat.id, pdf_file)
+
 
 @bot.message_handler(func=lambda msg: True)
 def send_all(message):
